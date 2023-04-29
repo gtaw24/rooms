50aed59609b78cb3df3368c946ff475aa4496b46
gtaw24 <131622601+gtaw24@users.noreply.github.com>
2023-04-29 18:37:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,84 @@
-var tables = {
-	101: {
-		status: 'available',
-		products: [],
-		total: 0
-	},
-	102: {
-		status: 'available',
-		products: [],
-		total: 0
-	},
-	103: {
-		status: 'available',
-		products: [],
-		total: 0
-	},
-	104: {
-		status: 'available',
-		products: [],
-		total: 0
-	},
-	105: {
-		status: 'available',
-		products: [],
-		total: 0
-	},
-	106: {
-		status: 'available',
-		products: [],
-		total: 0
-	},
-	107: {
-		status: 'available',
-		products: [],
-		total: 0
-	}
-};
+let tables = [
+  { name: "Table 101", status: "available", products: [] },
+  { name: "Table 102", status: "available", products: [] },
+  { name: "Table 103", status: "available", products: [] },
+  { name: "Table 104", status: "available", products: [] },
+  { name: "Table 105", status: "available", products: [] },
+  { name: "Table 106", status: "available", products: [] },
+  { name: "Table 107", status: "available", products: [] }
+];
 
-function toggleTableStatus(tableStatusId) {
-	var status = document.getElementById(tableStatusId);
-	var tableNumber = tableStatusId.replace('table', '');
-	var table = tables[tableNumber];
-
-	if (table.status === 'available') {
-		table.status = 'occupied';
-		status.innerHTML = 'Occupied';
-	} else {
-		table.status = 'available';
-		status.innerHTML = 'Available';
-	}
+function displayTables() {
+  let tableHtml = "";
+  for (let i = 0; i < tables.length; i++) {
+    let statusButton = "";
+    if (tables[i].status === "available") {
+      statusButton = `<button class="status-btn available" onclick="changeTableStatus(${i})">Available</button>`;
+    } else {
+      statusButton = `<button class="status-btn occupied" onclick="changeTableStatus(${i})">Occupied</button>`;
+    }
+    tableHtml += `
+      <div class="table">
+        <h3>${tables[i].name}</h3>
+        ${statusButton}
+        <div class="product-list" id="product-list-${i}">
+          <h4>Products:</h4>
+          <ul>
+            ${displayProducts(i)}
+          </ul>
+          <div>
+            <label for="product-name-${i}">Product Name:</label>
+            <input type="text" id="product-name-${i}" />
+          </div>
+          <div>
+            <label for="product-price-${i}">Product Price:</label>
+            <input type="number" id="product-price-${i}" />
+          </div>
+          <button onclick="addProduct(${i})">Add Product</button>
+        </div>
+        <div class="total" id="total-${i}">
+          <h4>Total: $${calculateTotal(i)}</h4>
+        </div>
+      </div>
+    `;
+  }
+  document.getElementById("tables-container").innerHTML = tableHtml;
 }
 
-function addProductToTable(tableNumber, productName, productPrice) {
-	var table = tables[tableNumber];
-	table.products.push({ name: productName, price: productPrice });
-	table.total += productPrice;
-
-	var productList = document.getElementById('table' + tableNumber + 'productlist');
-	var productItem = document.createElement('li');
-	productItem.innerHTML = productName + ' - $' + productPrice.toFixed(2);
-	productList.appendChild(productItem);
-
-	var total = document.getElementById('table' + tableNumber + 'total');
-	total.innerHTML = '$' + table.total.toFixed(2);
+function displayProducts(tableIndex) {
+  let productHtml = "";
+  for (let i = 0; i < tables[tableIndex].products.length; i++) {
+    productHtml += `
+      <li>${tables[tableIndex].products[i].name} - $${tables[tableIndex].products[i].price}</li>
+    `;
+  }
+  return productHtml;
 }
 
+function addProduct(tableIndex) {
+  let productName = document.getElementById(`product-name-${tableIndex}`).value;
+  let productPrice = document.getElementById(`product-price-${tableIndex}`).value;
+  if (productName && productPrice) {
+    tables[tableIndex].products.push({ name: productName, price: productPrice });
+    displayTables();
+  }
+}
+
+function calculateTotal(tableIndex) {
+  let total = 0;
+  for (let i = 0; i < tables[tableIndex].products.length; i++) {
+    total += parseFloat(tables[tableIndex].products[i].price);
+  }
+  return total.toFixed(2);
+}
+
+function changeTableStatus(tableIndex) {
+  if (tables[tableIndex].status === "available") {
+    tables[tableIndex].status = "occupied";
+  } else {
+    tables[tableIndex].status = "available";
+  }
+  displayTables();
+}
+
+displayTables();
